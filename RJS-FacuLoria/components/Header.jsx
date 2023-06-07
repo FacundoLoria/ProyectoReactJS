0e21ed01dfdd80b89641419a9d972858b042f2d0
Facu Loria <facundo.loria@gmail.com>
2023-06-07 12:36:54 -0300

    Agregado logo y carrito

--- RJS-FacuLoria/components/Header.jsx
+++ RJS-FacuLoria/components/Header.jsx
@@ -1,15 +1,19 @@
 import '../src/App.css'
+import React, { useState } from 'react';
+import Compra from "../components/botones"
 
-const Header = () => {  
+const Header = () => { 
     return (
         <header className="header">
             <div className="header_container">
-            <h1 className="header_logo">Facu Loria</h1>
+            <h1 className="header_logo"><img src="./public/PapirosNorte.png" alt="Carrito"/></h1>
 
                 <nav className="header_nav">
                     <a href="a" className='links'>Inicio</a>
                     <a href="b" className='links'>Productos</a>
                     <a href="c" className='links'>Contacto</a>
+                    <img src="./public/carrito.png" alt="Carrito"/>
+                    <p>199</p>
                 </nav>
 
             </div>
