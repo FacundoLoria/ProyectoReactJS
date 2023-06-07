8473a570247a9497ba9e399e164227903d59e3fd
Facu Loria <facundo.loria@gmail.com>
2023-06-07 15:00:02 -0300

    Agregado de productos

--- RJS-FacuLoria/components/ItemsListContainer.jsx
+++ RJS-FacuLoria/components/ItemsListContainer.jsx
@@ -4,12 +4,23 @@ const ItemListContainer = () => {
 
     return(
         <div className="lista_productos">
-            <h2>Lista de viajes</h2>
-        <hr/>
-        <img src="./public/escalera.jpg" alt="Bienvenido al infierno" />
-        <p>Viaje al infierno </p>
-        <hr/>
+            <h2>Lista de productos</h2>
+            <hr/>
+            <div className='productos'>
+        
+                 <img src="./public/productos/pote_55_natural.jpg" alt="pote 55 natural" />
+                    <p>Pote 55 - Natural</p>
 
+                <img src="./public/productos/pote_140_natural.jpg" alt="pote 140        natural" />
+                    <p>Pote 140 - Natural</p>
+
+                <img src="./public/productos/pote_230_natural.jpg" alt="pote 230 natural" />
+                    <p>Pote 230 - Natural</p>
+
+                <img src="./public/productos/pote_800_natural.jpg" alt="pote 800 natural" />
+                    <p>Pote 800 - Natural</p>
+            </div>
+        <hr/>
         </div>
     )   
 }
